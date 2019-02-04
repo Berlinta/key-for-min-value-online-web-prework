bc2cb1d90bfc49795f3bc3681616c48161ddf16c
Nash Brancic <nashfft@gmail.com>
2019-02-04 21:39:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+end
+small_v = nil 
+small_k = nil
+name_hash.each do |k, v|
+  if small_v = nil || value < small_v
+    small_k = k
+    small_v = v
+  end
+end
+small_v
 end
