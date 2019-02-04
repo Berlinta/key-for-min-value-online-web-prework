308d5511ba7a9e6f8beaff40cb74dbce1745a2a3
Nash Brancic <nashfft@gmail.com>
2019-02-04 21:53:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@ def key_for_min_value(name_hash)
   small_key = nil
   small_value = nil
   name_hash.each do |key, value|
-    if small_value == nil || value < small_key
+    if small_value == nil || value < small_value
       small_key = key
       small_value = value
     end
