1e3dae58d4bde2bdf764a90a91880262dba75d85
Nash Brancic <nashfft@gmail.com>
2019-02-04 21:49:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+def key_for
   
   smallest_key = nil
   smallest_value = nil
